40fa52f5395e5f3bb914bf3c87ff2af0410514ca
bogdofreewoman <bsadchenko@gmail.com>
2023-01-19 20:57:39 +0100

    govno

--- php/reg.php
+++ php/reg.php
@@ -11,27 +11,24 @@ $password_conf = $_POST['password_conf'];
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 $success = true;
 
-$sql = "SELECT Email FROM User";
-$result = $conn->query($sql);
+$sql = mysqli_query("SELECT Email FROM User;");
+$sql = mysqli_fetch_assoc($sql);
 
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]."Email" . $row["Email"]. "<br>";
-}
+var_dump();
 die();
 
-if (empty($password) || empty($password_conf)) {
-    $success = false;
-    $_SESSION['msg-err'] = 'You need enter password and confirm password inputs!';
-}
-if ($password !== $password_conf) {
-    $success = false;
-    $_SESSION['msg-err'] ='Password is not correct';
-}
-
-if ($success) {
-    $_SESSION['msg-succ'] ='You Registered';
-    mysqli_query($conn , "INSERT INTO `User` (`Email`,`LastName`,`FirstName`,`Password`) VALUES ('$email','$f_name','$s_name','$password_hash')");
-}
-header('Location: ../login.php');
+//if (empty($password) || empty($password_conf)) {
+//    $success = false;
+//    $_SESSION['msg-err'] = 'You need enter password and confirm password inputs!';
+//}
+//if ($password !== $password_conf) {
+//    $success = false;
+//    $_SESSION['msg-err'] ='Password is not correct';
+//}
+//
+//if ($success) {
+//    $_SESSION['msg-succ'] ='You Registered';
+//    mysqli_query($conn , "INSERT INTO `User` (`Email`,`LastName`,`FirstName`,`Password`) VALUES ('$email','$f_name','$s_name','$password_hash')");
+//}
+//header('Location: ../login.php');
